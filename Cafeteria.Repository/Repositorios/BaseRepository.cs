96f8ad724aabb94452285056a079ae482a30f474
PauloHNDS <ps0465882@gmail.com>
2023-08-12 17:39:43 -0300

    adicionando biblioteca no projeto.

---
+++ Cafeteria.Repository/Repositorios/BaseRepository.cs
@@ -0,0 +1,35 @@
+﻿
+using Cafeteria.Business;
+using Cafeteria.Repository.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cafeteria.Repository.Repositorios
+{
+    public class BaseRepository<TEntity> where TEntity : class
+    {
+        private readonly Contexto _contexto;
+        private readonly DbSet<TEntity> _dbSet;
+
+        public BaseRepository(Contexto contexto)
+        {
+            _contexto = contexto;
+            _dbSet = contexto.Set<TEntity>();
+        }
+
+        public virtual async Task<List<TEntity>> Get() => await _dbSet.ToListAsync();
+        
+        public virtual async Task<TEntity?> Get(int Id) => await _dbSet.FindAsync(Id);
+
+        public virtual async Task Delete(int Id)
+        {
+            var entity = await _dbSet.FindAsync(Id);
+
+            if (entity is not null)
+            {
+                _dbSet.Remove(entity);
+            }
+        }
+
+        public virtual void Update(TEntity entity) => _contexto.Entry(entity);
+    }
+}
---
+++ Cafeteria.Repository/Repositorios/EstruturaRepository.cs
@@ -0,0 +1,12 @@
+﻿using Cafeteria.Business;
+using Cafeteria.Repository.Context;
+
+namespace Cafeteria.Repository.Repositorios
+{
+    public class EstruturaRepository : BaseRepository<Estrutura>
+    {
+        public EstruturaRepository(Contexto contexto) : base(contexto)
+        {
+        }
+    }
+}
